b5154329c2c9a05bbefbe95e1ecb05434b87b4f3
Binod Aryal <binodaryal21@gmail.com>
2015-01-23 13:19:57 +0545

    println program

---
+++ PrintlnPicture.java
@@ -0,0 +1,16 @@
+public class PrintlnPicture
+{
+    public static void main(String[] args)
+    {
+        System.out.println("              '____'");
+        System.out.println("              ( ©© )");
+        System.out.println("   /============\\/");
+        System.out.println("  / ||  %%  ||");
+        System.out.println(" *  ||------||");
+        System.out.println("    ¥¥      ¥¥ ");
+        System.out.println("    \"\"      \"\"");
+        
+        
+        
+    }
+}
